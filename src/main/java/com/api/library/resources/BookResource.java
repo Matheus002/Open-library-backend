13e62ea050c12ef92d44e4b6f3ed7f3e57d60f5f
Matheus Henrique Oliveira Santos <matheus@MacBook-Air-de-Matheus.local>
2021-11-24 05:56:23 -0300

    chore: add mysql connection

--- src/main/java/com/api/library/resources/BookResource.java
+++ src/main/java/com/api/library/resources/BookResource.java
@@ -1,8 +1,24 @@
 package com.api.library.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.library.domain.Book;
+import com.api.library.services.BookService;
+
 @RestController
+@RequestMapping(value = "/books")
 public class BookResource {
+	
+	@Autowired
+	private BookService service;
+	
+	public ResponseEntity<Book> find(@PathVariable Long id) {
+		Book obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+	}
 
 }
--- src/main/java/com/api/library/services/BookService.java
+++ src/main/java/com/api/library/services/BookService.java
@@ -1,9 +1,13 @@
 package com.api.library.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.library.domain.Book;
 import com.api.library.repositories.BookRepository;
+import com.api.library.resources.exception.ObjectNotFoundException;
 
 @Service
 public class BookService {
@@ -11,6 +15,9 @@ public class BookService {
 	@Autowired
 	private BookRepository repo;
 	
-	private Book find(Integer)
+	public Book find(Long id) {
+		Optional<Book> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: "+ id));
+	}
 
 }
